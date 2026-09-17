1f1c8681dc14c92aa79722ec189cf953aa22bab2
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-17 18:55:05 +0800

    Create DDP11.java

---
+++ DDP11.java
@@ -0,0 +1,25 @@
+public class DDP11 {
+    
+    public static void main(String[] args) {
+        
+        String nama = "Abdul Rahman";
+        String nim = "D0226035";
+        int semester = 1;
+        float ipk = 3.99;
+        char kelas = 'D';
+        booleand aktif = true;
+        
+        System.out.println("Nama \t: " + nama);
+        System.out.println("NIM  \t: " + nim);
+        System.out.println("Semester  \t: " + senester);
+        System.out.println("IPK  \t: " + ipk);
+        System.out.println("Kelas  \t: " + kelas);
+        System.out.println("Aktif  \t: " + aktif);
+        
+        
+        
+        
+        
+    }
+    
+}
