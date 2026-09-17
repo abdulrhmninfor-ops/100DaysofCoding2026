6f620127280928f637bc72d00abfb16d69c6c3c3
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-17 18:28:17 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,13 @@
+public class DDP9 {
+    
+    public static void main(String[] args) {
+        
+        final String NAMA_KAMPUS = "Universitas Sulawesi Barat";
+        final String KODE_KAMPUS = "UC01";
+        
+        System.out.println("Nama Kampus \t: " + NAMA_KAMPUS);
+        System.out.println("Kode Kampus \t: " + KODE_KAMPUS);
+        
+    }
+    
+}
