53d611779440f5c9477693a74449142c6f90ba47
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-17 18:39:00 +0800

    Create DDP10.java

---
+++ DDP10.java
@@ -0,0 +1,21 @@
+public class DDP10 {
+    
+    public static void main(String[] args) {
+        
+        final String namaUniversitas = "Universitas Sulawesi Barat";
+        final String namaFakultas = "Teknik";
+        
+        String nama = "Abdul Rahman";
+        String nim = "D0226035";
+        int semester = "1";
+        
+        System.out.println("Universitas \t: " + namaUniversitas);
+        System.out.println("Fakuktas \t: " + namaFakultas);
+        System.out.println("Nama \t: " + nama);
+        System.out.println("NIM  \t: " + nim);
+        System.out.println("Semester  \t: " + senester);
+        
+        
+    }
+    
+}
